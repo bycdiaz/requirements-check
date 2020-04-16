54cb7f2d8711ef502f752cca58726c578b211ca0
Carlos Diaz <cdiaz86@gmail.com>
2020-04-16 17:55:45 -0400

    Cleans up regex.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 
-gem 'fileutils', '~> 0.7.2'
+# gem 'fileutils', '~> 0.7.2'
 gem 'csv'
--- requirements_check.rb
+++ requirements_check.rb
@@ -1,28 +1,30 @@
-require 'fileutils'
+# require 'fileutils'
 require 'csv'
-
-
 
 def collect_ids
   student_info = Hash.new
 
   CSV.foreach('data/STU-Student Transcript-ALL-BIOMED_201925.csv', headers: true) do |header|
-    advisors = ["Laurie Lenz", "Caryn Glaser", "Carlos Diaz", "Elise Bryers"]
 
     if !(student_info.include?(header["Univ Id"]))
       student_info[header["Univ Id"]] = Hash.new
+
       student_info[header["Univ Id"]]["Admit Year"] = header["Term Code Admit"]
       student_info[header["Univ Id"]]["Name"] = header["Student"]
 
-      if header["Last Name"].start_with?(/[ABCDEF]/)
-        student_info[header["Univ Id"]]["Advisor"] = advisors[0]
-      elsif header["Last Name"].start_with?(/[GHIJK]/)
-        student_info[header["Univ Id"]]["Advisor"] = advisors[1]
-      elsif header["Last Name"].start_with?(/[LMNOPQ]/)
-        student_info[header["Univ Id"]]["Advisor"] = advisors[2]
-      elsif header["Last Name"].start_with?(/[RSTUVWXYZ]/)
-        student_info[header["Univ Id"]]["Advisor"] = advisors[3]
+      if header["Last Name"].start_with?(/[A-F]/)
+        student_info[header["Univ Id"]]["Advisor"] = "Laurie Lenz"
+      elsif header["Last Name"].start_with?(/[G-K]/)
+        student_info[header["Univ Id"]]["Advisor"] = "Caryn Glaser"
+      elsif header["Last Name"].start_with?(/[L-Q]/)
+        student_info[header["Univ Id"]]["Advisor"] = "Carlos Diaz"
+      elsif header["Last Name"].start_with?(/[R-Z]/)
+        student_info[header["Univ Id"]]["Advisor"] = "Elise Bryers"
       end
+
+      # this needs to be in a second run of the CSV
+      student_info["Courses"] = Hash.new
+      student_info["Courses"][header["Crse"]] = header["Grade"]
     end
   end
   student_info
