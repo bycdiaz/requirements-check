fc9db734d3b1cc73372e0ac43b62ab02c7d8262b
Carlos Diaz <cdiaz86@gmail.com>
2020-04-16 14:50:14 -0400

    Identifies Academic Advisor.

--- requirements_check.rb
+++ requirements_check.rb
@@ -7,16 +7,22 @@ def collect_ids
   student_info = Hash.new
 
   CSV.foreach('data/STU-Student Transcript-ALL-BIOMED_201925.csv', headers: true) do |header|
-    # advisors = ["Carlos Diaz", "Homer Simpson"]
+    advisors = ["Laurie Lenz", "Caryn Glaser", "Carlos Diaz", "Elise Bryers"]
 
     if !(student_info.include?(header["Univ Id"]))
       student_info[header["Univ Id"]] = Hash.new
       student_info[header["Univ Id"]]["Admit Year"] = header["Term Code Admit"]
       student_info[header["Univ Id"]]["Name"] = header["Student"]
 
-      # if header["Last Name"].start_with?("C")
-      #   student_info[header["Univ Id"]]["Advisor"] = advisors[0]
-      # end
+      if header["Last Name"].start_with?(/[ABCDEF]/)
+        student_info[header["Univ Id"]]["Advisor"] = advisors[0]
+      elsif header["Last Name"].start_with?(/[GHIJK]/)
+        student_info[header["Univ Id"]]["Advisor"] = advisors[1]
+      elsif header["Last Name"].start_with?(/[LMNOPQ]/)
+        student_info[header["Univ Id"]]["Advisor"] = advisors[2]
+      elsif header["Last Name"].start_with?(/[RSTUVWXYZ]/)
+        student_info[header["Univ Id"]]["Advisor"] = advisors[3]
+      end
     end
   end
   student_info
